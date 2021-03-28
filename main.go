41ebd79c73c11d603dcd7103298974b9e4cddaf4
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-28 16:52:34 +1100

    Use cobra

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/spf13/cobra"
+)
+
+var commands = []string{
+	"init",
+	"sign",
+}
+
+func main() {
+	cmd := &cobra.Command{
+		Use: fmt.Sprintf("anchor %s", strings.Join(commands, " | ")),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return fmt.Errorf(("unimplemented"))
+		},
+	}
+	err := cmd.RunE(cmd, os.Args)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
